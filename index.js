89168925e40bbc6a5d4159f457bcf516cd177c5b
paolorevillosa <revillosakristianpaolo@gmail.com>
2021-03-23 19:44:03 +0800

    a

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  
-  const details = exec.exec('git describe --tags --abbrev=0')
+  const details = exec.exec('git log $(git describe --tags --abbrev=0)..HEAD    --pretty=format:\'{%n  "commit": "%H",%n  "author": "%aN <%aE>",%n  "date": "%ad",%n  "message": "%f"%n},\'     $@ |     perl -pe \'BEGIN{print "["}; END{print "]\n"}\' |     perl -pe \'s/},]/}]/')
+
+
   console.log(`The details: ${details}`);
 } catch (error) {
   core.setFailed(error.message);
