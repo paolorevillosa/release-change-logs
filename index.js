389a819b42d54261bf0f400c763f8b061232942e
Kristian Paolo Revillosa <revillosakristianpaolo@gmail.com>
2021-03-23 19:26:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -10,6 +11,9 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  
+  const details = exec('git describe --tags --abbrev=0')
+  console.log(`The details: ${details}`);
 } catch (error) {
   core.setFailed(error.message);
 }
