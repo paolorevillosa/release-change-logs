64e4fd5033326685a6c0002e8cbfafa53cc8191e
paolorevillosa <revillosakristianpaolo@gmail.com>
2021-03-23 20:25:31 +0800

    test

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ try {
 
   const latestRelease = exec.exec('git describe --tags --abbrev=0');
   
-  const logScript = 'git log '+ latestRelease +'..HEAD';
+  const logScript = 'git log v3..HEAD';
   const logs = exec.exec(logScript)
   console.log(`The logs: ${logs}`);
 } catch (error) {
