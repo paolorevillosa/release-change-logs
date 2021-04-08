7e24e228814b10f94e47396ea4e31a462d0555ab
paolorevillosa <revillosakristianpaolo@gmail.com>
2021-04-08 18:42:52 +0800

    error on .toLowerCase function

--- index.js
+++ index.js
@@ -78,6 +78,7 @@ async function parseLogsJson(logs){
       splitMessage.shift();
       var message = splitMessage.join(" ");
       feature.push(new Array(type, id, message, log.author, log.original_message));
+      continue;
     }
     
     if(splitMessage[0].toLowerCase() == bugTag.toLowerCase()){
@@ -88,6 +89,7 @@ async function parseLogsJson(logs){
       splitMessage.shift();
       var message = splitMessage.join(" ");
       bug.push(new Array(type, id, message, log.author, log.original_message));
+      continue;
     }
 
     if(bugTag.toLowerCase() == "b" && splitMessage[0].toLowerCase() == "cb"){
@@ -98,6 +100,7 @@ async function parseLogsJson(logs){
       splitMessage.shift();
       var message = splitMessage.join(" ");
       bug.push(new Array(type, id, message, log.author, log.original_message));
+      continue;
     }
     
   }
