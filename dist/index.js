b6f0aa826aeb67ebe282ada534a946dc7b292807
paolorevillosa <revillosakristianpaolo@gmail.com>
2023-02-21 12:14:14 +0800

    addedTest

--- dist/index.js
+++ dist/index.js
@@ -48,8 +48,8 @@ async function main() {
 
     // core.setOutput('latest_tag', latestRelease);
     // core.setOutput('logs-on-json', logs);
-    // core.setOutput('change-logs', changeLogMessage);
-    // core.setOutput('logs-on-text-file', "logs.txt");
+    core.setOutput('change-logs', changeLogMessage);
+    core.setOutput('logs-on-text-file', "logs.txt");
   } catch (error) {
     core.setFailed(error.message);
   }  
